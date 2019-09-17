45fa862e47886dc5ed2f0c369b3b4a5783598f45
honwaii <honwaii@126.com>
2019-09-17 22:34:21 +0800

    renew demo

---
+++ src/main/java/com/h3c/iot/wordcount/ClusterDemo.java
@@ -0,0 +1,50 @@
+package com.h3c.iot.wordcount;
+
+import akka.actor.AbstractActor;
+import akka.actor.UntypedActor;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent;
+import akka.cluster.Member;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+public class ClusterDemo extends UntypedActor {
+    Cluster cluster = Cluster.get(getContext().system());
+
+
+    @Override
+    public void preStart() throws Exception, Exception {
+        cluster.subscribe(getSelf(), ClusterEvent.initialStateAsEvents(),
+                ClusterEvent.UnreachableMember.class, ClusterEvent.MemberEvent.class);
+        super.preStart();
+    }
+
+    @Override
+    public void postStop() throws Exception, Exception {
+        cluster.unsubscribe(getSelf());
+        super.postStop();
+    }
+
+    public void onReceive(Object message) throws Exception, Exception {
+        if (message instanceof ClusterEvent.MemberUp) {
+            ClusterEvent.MemberUp mUp = (ClusterEvent.MemberUp) message;
+            Member m = mUp.member();
+            System.out.println(getSelf() + "-->Member " + m + "is Up: the role is " + m.roles());
+        } else if (message instanceof ClusterEvent.UnreachableMember) {
+            ClusterEvent.UnreachableMember mUnreachable = (ClusterEvent.UnreachableMember) message;
+            Member m = mUnreachable.member();
+            System.out.println(getSelf() + "-->Member " + m + "detected as unreachable: the role is  " + m.roles());
+        } else if (message instanceof ClusterEvent.MemberRemoved) {
+            ClusterEvent.MemberRemoved uRemoved = (ClusterEvent.MemberRemoved) message;
+            Member m = uRemoved.member();
+            System.out.println(getSelf() + "-->Member " + m + "is removed: the role is  " + m.roles());
+        } else if (message instanceof ClusterEvent.MemberEvent) {
+            ClusterEvent.MemberEvent me = (ClusterEvent.MemberEvent) message;
+            Member m = me.member();
+            System.out.println(getSelf() + "-->MemberEvent: " + m + " " + m.roles());
+        } else {
+            System.out.println(getSelf() + "-->other: " + message);
+            unhandled(message);
+        }
+    }
+}
